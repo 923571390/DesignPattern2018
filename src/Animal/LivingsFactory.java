db343aa718889c12a58250328e3c2434d77186c4
923571390 <923571390@qq.com>
2018-10-31 17:29:17 +0800

    Update LivingsFactory.java

--- src/Animal/LivingsFactory.java
+++ src/Animal/LivingsFactory.java
@@ -5,5 +5,5 @@ import Plant.Plant;
 public interface LivingsFactory {
     public Plant createPlant();
     public Animal createAnimal();
-    //这是抽象工厂类，使用Abstract Factory模式
+    //This is the abstract factory class.
 }
