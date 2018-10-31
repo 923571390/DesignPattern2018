82fa6a1af00d59e4dd3082324af3c5d0aa8e6f04
okitakagura <354207983@qq.com>
2018-10-31 12:58:24 +0800

    update

---
+++ src/Test/BridgeTest.java
@@ -0,0 +1,36 @@
+package Test;
+
+import Fertilizer.AdvancedFertilizer;
+import Fertilizer.CommonFertilizer;
+import Fertilizer.FertilizerForPlant;
+import Fertilizer.FertilizerType;
+import Plant.Bamboo;
+import Plant.Grass;
+import Plant.Plant;
+
+
+public class BridgeTest {
+	public static void main(String[] args) {
+		Plant bamboo = new Bamboo();
+		Plant grass = new Grass();
+		//register in prototype
+		FertilizerForPlant.addPrototype(new CommonFertilizer(1));
+		FertilizerForPlant.addPrototype(new AdvancedFertilizer(1));
+	
+		//test prototype
+		FertilizerForPlant f1 = FertilizerForPlant.findAndClone(FertilizerType.COMMON);
+		FertilizerForPlant f2 = FertilizerForPlant.findAndClone(FertilizerType.COMMON);
+		FertilizerForPlant f3 = FertilizerForPlant.findAndClone(FertilizerType.ADVANCED);
+		FertilizerForPlant f4 = FertilizerForPlant.findAndClone(FertilizerType.ADVANCED);
+		
+		//test bridge
+		f1.fertilizing(grass);
+		f1.fertilizing(bamboo);
+		f2.fertilizing(bamboo);
+		f2.fertilizing(grass);
+		f3.fertilizing(bamboo);
+		f4.fertilizing(grass);
+		
+   
+    }
+}
---
+++ src/Test/CommandTest.java
@@ -0,0 +1,23 @@
+package Test;
+
+import UndoAndRedo.CommandManager;
+import UndoAndRedo.CutCommand;
+import UndoAndRedo.InsertCommand;
+
+public class CommandTest {
+
+	public static void main(String[] args) {
+		//test command
+		CommandManager commandManager = new CommandManager();
+		commandManager.executeCommand(new InsertCommand("bamboo"));
+		commandManager.executeCommand(new InsertCommand("seed"));
+		commandManager.executeCommand(new InsertCommand("seed"));
+		commandManager.executeCommand(new InsertCommand("bamboo"));
+		commandManager.executeCommand(new CutCommand());
+				
+		commandManager.undo();
+		commandManager.undo();
+		commandManager.redo();
+		commandManager.redo();
+	}
+}
