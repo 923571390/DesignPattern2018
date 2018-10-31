591c14704f28c0148418a47a2a91c68632d82a23
923571390 <923571390@qq.com>
2018-10-31 17:41:58 +0800

    Update Mediator.java

--- src/Feeder/Mediator.java
+++ src/Feeder/Mediator.java
@@ -5,6 +5,6 @@ import Building.House;
 import StoreHouse.StoreHouse;
 
 public abstract class Mediator {
-	//This is the abstract mediator,we use Mediator to realize the feed-animal function
+	//This is the abstract mediator,we use Mediator to complete the feed-animal function
 	public abstract void feed(House animalhouse, Farm farm);
 }
