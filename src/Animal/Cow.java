14caafa54105c4c6fb22384b2481e1a969fcaab4
923571390 <923571390@qq.com>
2018-10-31 17:15:33 +0800

    Update Cow.java

--- src/Animal/Cow.java
+++ src/Animal/Cow.java
@@ -3,11 +3,12 @@ package Animal;
 import Building.Farm;
 
 public class Cow extends Animal{
-	
+	//concrete product class cow
 	public void create(Farm farm) {
 		System.out.println("Cow is created!");
 		farm.getCowshed().addEntity(this);
 	}
+	//to create a cow
 
 	@Override
 	public void update() {
