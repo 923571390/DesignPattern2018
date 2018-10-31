99610fcd6205d1f6b290733859bcc554b5a0d037
923571390 <923571390@qq.com>
2018-10-31 17:18:02 +0800

    Update Create.java

--- src/Animal/Create.java
+++ src/Animal/Create.java
@@ -10,30 +10,34 @@ public class Create {
 		LivingsFactory factoryGroup1 = FactoryGroup1.getInstance();
 		Animal cow = factoryGroup1.createAnimal();
 		cow.create(farm); 
-		return cow; //创建奶牛	
+		return cow; 
+		//create a cow
 	}
 	
 	public static Plant grass(Farm farm) {
 		
 		LivingsFactory factoryGroup1 = FactoryGroup1.getInstance();
 		Plant grass = factoryGroup1.createPlant();
-		grass.create(farm); //创建牧草
+		grass.create(farm); 
 		return grass;
+		//create a grass
 	}
 	
 	public static Animal rhizomy(Farm farm) {
 		
 		LivingsFactory factoryGroup2 = FactoryGroup2.getInstance();	
 		Animal rhizomy = factoryGroup2.createAnimal();
-		rhizomy.create(farm); //创建竹鼠
+		rhizomy.create(farm); 
 		return rhizomy;
+		//create a rhizomy
 	}	
 	
 	public static Plant bamboo(Farm farm) {
 		
 		LivingsFactory factoryGroup2 = FactoryGroup2.getInstance();	
 		Plant bamboo = factoryGroup2.createPlant();
-		bamboo.create(farm); //创建竹子
+		bamboo.create(farm); 
 		return bamboo;
+		//create a bamboo
 	}
 }
